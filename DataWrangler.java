40101a912a9d50b2bd33b1fbe4c04d6c6d116219
Rex Chen <68821163+rchen266@users.noreply.github.com>
2020-10-17 16:42:32 -0500

    Add files via upload

---
+++ DataWrangler.java
@@ -0,0 +1,43 @@
+// --== CS400 File Header Information ==--
+// Name: Rex Chen
+// Email: rchen266@wisc.edu
+// Team: AD
+// TA: Sophie Stephenson
+// Lecturer: Florian
+// Notes to Grader: <optional extra notes>
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DataWrangler {
+    public static boolean load(RedBlackTree<Movie> movies) {
+        try {
+            Scanner scanner = new Scanner(new File("Project 2 movies.txt"));
+            while (scanner.hasNext()) {
+                String t = scanner.nextLine();// get the title of the movie
+                String d = scanner.nextLine();// get the director of movie
+                String g = scanner.nextLine();// get the genre of the movie
+                int r = scanner.nextInt();// the runtime of the movie
+                int l = scanner.nextInt();// the lenghth of the actors table
+                scanner.nextLine();
+                String[] s = new String[l];
+                for (int i = 0; i < l; i++)//
+                {
+                    s[i] = scanner.nextLine();
+                }
+                int yrs = scanner.nextInt();
+                double rating = scanner.nextDouble();
+                Movie m = new Movie(t, d, g, r, s, yrs, rating);
+                movies.insert(m);
+
+                scanner.nextLine();
+                scanner.nextLine();
+
+            }
+            return true;
+        } catch (IOException e) {
+            System.out.println("File not found.");
+            return false;
+        }
+    }
+}
---
+++ Movie.java
@@ -0,0 +1,47 @@
+// --== CS400 File Header Information ==--
+// Name: Rex Chen
+// Email: rchen266@wisc.edu
+// Team: AD
+// TA: Sophie Stephenson
+// Lecturer: Florian
+// Notes to Grader: <optional extra notes>
+public class Movie implements Comparable<Movie> {
+    private String title;
+    private String director;
+    private String genre;
+    private int runtime;
+    private String[] actor;
+    private int year;
+    private double ratings;
+
+    public Movie(String t, String d, String g, int rt, String[] act, int yr, double r) {
+        title = t;
+        director = d;
+        genre = g;
+        runtime = rt;
+        this.actor = act;
+        year = yr;
+        ratings = r;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return getTitle();
+    }
+
+    @Override
+    public int compareTo(Movie m) {
+        if (title.compareTo(m.getTitle()) > 0) {
+            return 1;
+        }
+        if (title.compareTo(m.getTitle()) < 0) {
+            return -1;
+        }
+        return 0;
+    }
+
+}
